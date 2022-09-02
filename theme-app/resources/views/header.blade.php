cdbc72c598f202df39f08d974ea5904e4c32015b
Александр Логодзинский <87911051+Logodzinskii@users.noreply.github.com>
2022-09-02 13:03:42 +0500

    delete

--- theme-app/resources/views/header.blade.php
+++
@@ -1,8 +0,0 @@
-<header class="p-0 m-0">
-    <section class="container" style="height: 10vh">
-        <div class="row">
-            <div class="col-lg-10">Компания-тема</div>
-            <div class="col-lg-2">+7 963 272 72 82</div>
-        </div>
-    </section>
-</header>
