82581ec0313f75a509e286c91c01ed0cdd3a4410
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-09-06 22:39:27 +0500

    Калькулятор переработаны формы вывода данных Добавлена форма обратной связи устранение ошибок

--- theme-app/routes/web.php
+++ theme-app/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Jenssegers\Agent\Agent;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
