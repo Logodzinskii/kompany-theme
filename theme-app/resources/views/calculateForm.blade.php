9fcedfb681b0f625eb34906c23f5833f11d15767
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-09-07 20:07:45 +0500

    Калькулятор добавлены цвета для форм

--- theme-app/resources/views/calculateForm.blade.php
+++ theme-app/resources/views/calculateForm.blade.php
@@ -1,17 +1,17 @@
-<section class="d-flex justify-content-center row col-12">
+<section class="d-flex justify-content-center align-items-center row col-12" style="min-height: 80vh">
     <h1 class="text-center">Форма обратной связи</h1>
     <form class="col-12 col-md-8 col-lg-8" method="post" action='order/'>
         @csrf
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <label for="exampleInputEmail1" class="form-label">Email для связи с вами</label>
             <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <div id="emailHelp" class="form-text">Мы не присылаем рекламу и спам</div>
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <label for="exampleFormControlTextarea1" class="form-label">Дополнительная информация</label>
             <textarea class="form-control" name="body" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Отправить</button>
     </form>
     <p></p>
 </section>
--- theme-app/resources/views/calculateSelf.blade.php
+++ theme-app/resources/views/calculateSelf.blade.php
@@ -66,21 +66,21 @@
                 </div>
             </div>
         </section>
-<h1 class="text-center"> 2 ШАГ - Укажите размеры вашей кухни </h1>
-        <section class="col-12 col p-0 m-0 col-lg-12 d-flex justify-content-around flex-wrap">
+        <h1 class="text-center"> 2 ШАГ - Укажите размеры вашей кухни </h1>
+        <section class="col-12 col m-0 col-lg-12 d-flex justify-content-around flex-wrap bg-primary" style="min-height: 80vh; padding-top: 20px; padding-bottom: 20px; padding-left: 0; padding-right: 0;">
             <div class="col-12 col-lg-8 d-flex justify-content-around flex-wrap">
                 @foreach($parametrs as $param)
                     <div class="card d-flex row justify-content-center flex-wrap" style="min-width: 30vw">
-                        <h5 class="card-title">{{ $param['titleCard'] }}&nbsp;</h5>
-                        <div class="input-group mb-3">
+                        <h5 class="card-title bg-dark bg-gradient text-white" style="border-radius: 3px; padding: 10px 0px 10px 0px">{{ $param['titleCard'] }}&nbsp;</h5>
+                        <div class="input-group mb-3 bg-white text-dark">
                             <span class="input-group-text" id="basic-addon1">м</span>
                             <input type="number" name="param" class="form-control" placeholder="{{ $param['placeholderInput'] }}" aria-label="Длинна, мм" aria-describedby="basic-addon1">
                         </div>
-                        <div>цена за погонный метр - <span class="info card-text" id="{{ $param['id'] }}">{{ $param['initPrice'] }}</span></div>
-                        <div>цена с учетом размеров - <span id="tatalpriceId{{ $param['id'] }}">0</span></div>
+                        <div>цена за погонный метр - <span class="info card-text text-secondary" id="{{ $param['id'] }}">{{ $param['initPrice'] }}</span></div>
+                        <div class="bg-warning text-dark" style="border-radius: 3px; padding: 10px 0px 10px 0px">цена с учетом размеров - <span id="tatalpriceId{{ $param['id'] }}">0</span></div>
                     </div>
                 @endforeach
-                <div class="card d-flex row justify-content-center flex-wrap">
+                <div class="card d-flex row justify-content-center flex-wrap text-center bg-success text-white">
                     <h5 class="card-title" style="min-width: 50vw">Итоговая цена</h5>
                     <h1 id="totalPrice" class="info card-text">0</h1>
                     <a href="#" class="btn btn-secondary">Сохранить</a>
