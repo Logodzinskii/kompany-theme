da12d0cdba2e47ef318b21cc3bd8b490a1081136
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-09-15 00:12:18 +0500

    Добавлены все элементы в калькулятор

--- theme-app/public/js/calc_v2.js
+++ theme-app/public/js/calc_v2.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
 
     });
 
+
     $('.checkbox').on("click", function () {
         if ($(this).is(":checked")) {
             $(this).parent().parent().children().last().attr('type',  'text');
@@ -21,16 +22,49 @@ $(document).ready(function () {
 
     function initialize()
     {
+        let name = 'name';
+        let email = 'name';
+        let mobile_number = 'name';
+        let message = 'name';
+        let _token   = $('meta[name="csrf-token"]').attr('content');
+        $.ajax({
+            url: "/calculate/",
+            type:"POST",
+            data:{
+                name:name,
+                email:email,
+                mobile_number:mobile_number,
+                message:message,
+                _token: _token
+            },
+            success:function(response){
+                console.log(response);
+                if(response) {
+                    //alert(response.success);
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
         setBox(177,313, 170,30, 'BoxTop');
         setBox(209,325, 170,48, 'BoxMiddle');
         setBox(319,484, 28,68, 'BoxDown');
+        setBox(329,469, 14,54, 'BottleMaker');
+        setBox(312,392, 38,54, 'BoxShelves');
+        setBox(303,364, 28,54, 'BoxDishwasher');
+        setBox(293,313, 52,54, 'BoxWashing');
+        setBox(150,274, 39,187, 'PenalFridge');
+        setBox(140,232, 42,187, 'PenalMicrowave');
+        setBox(124,154, 79,187, 'PenalShelves');
     }
+
     initialize();
+
     function setBox(top,left, width, height, name)
     {
         $('.' + name).css({"top": ""+ top +"px", "left": ""+ left +"px", "width": ""+ width +"px", "height": ""+ height +"px"});
     }
-
 
     function getPosition(e) {
         var x = y = 0;
