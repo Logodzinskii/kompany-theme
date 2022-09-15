475137c88c0207dfe139da8e39e1c9d9192bfa2a
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-09-15 23:16:19 +0500

    Откоректированы стили и кнопки калькулятора

--- theme-app/resources/views/calculator/calcForm.blade.php
+++ theme-app/resources/views/calculator/calcForm.blade.php
@@ -16,26 +16,30 @@
     </head>
     <body class="container-fluid p-0 m-0">
         @include('header')
-        <section class="calc_container d-flex justify-content-between flex-wrap">
+        <section class="calc_container d-flex col justify-content-between flex-wrap">
             <div class="cel calc_body">
-                <img src="{{asset('images/k2.jpg')}}" width="700" height="500" />
+
             </div>
-            <div class="d-flex justify-content-around row ">
-                <h1>Выберите стиль кухни</h1>
-                <form class="d-flex justify-content-around row flex-wrap">
+            <div class="d-flex justify-content-around row col-lg-4 col-xs-12 col-md-12">
+                <h4>Выберите комплектацию кухни</h4>
+                <form class="d-flex justify-content-around flex-wrap">
                     @foreach( $items as $item)
-                        <div class="bg-secondary rounded-2 text-white d-flex row" style="width: 20%; margin: 5px; opacity: 0.7">
+                        <div class="bg-secondary rounded-2 text-white d-flex col " style="margin: 5px; opacity: 0.7" data-box="{{$item['nameClassBox']}}">
                             <div class="col d-flex flex-nowrap">
                                 <input type="checkbox" class="checkbox" value="200" name="{{$item['nameClassBox']}}">
                                 <label class="p-1" for="{{$item['nameClassBox']}}">{{$item['nameBoxBottom']}}</label>
                             </div>
-                            <input type="text" name="{{$item['nameClassBox']}}" id="login" placeholder="{{$item['placeholder']}}" value="">
+                            <input type="number" name="{{$item['nameClassBox']}}" id="login" placeholder="{{$item['placeholder']}}" value="" disabled>
                         </div>
                         <div class="{{$item['typeBox']}} {{$item['nameClassBox']}}"></div>
                     @endforeach
                 </form>
             </div>
         </section>
-
+        <section class="d-flex justify-content-center row">
+            <h2 class="text-center">Предварительная стоимость составляет</h2>
+            <p class="text-center">250 000</p>
+            <button type="submit" class="btn btn-primary w-25" >Отправить</button>
+        </section>
         @include('footer')
     </body>
