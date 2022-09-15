4cfdb422fa1779422bf51743e418ecd3e5dd7ae1
Александр Логодзинский <87911051+Logodzinskii@users.noreply.github.com>
2022-09-15 15:12:25 +0500

    Добавлен расчет на изменение экрана для модулей блоков

--- theme-app/public/js/calc_v2.js
+++ theme-app/public/js/calc_v2.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
         //alert(getPosition(e));
 
     });
-
-
     $('.checkbox').on("click", function () {
         if ($(this).is(":checked")) {
             $(this).parent().parent().children().last().attr('type',  'text');
@@ -27,6 +25,7 @@ $(document).ready(function () {
         let mobile_number = 'name';
         let message = 'name';
         let _token   = $('meta[name="csrf-token"]').attr('content');
+
         $.ajax({
             url: "/calculate/",
             type:"POST",
@@ -38,25 +37,70 @@ $(document).ready(function () {
                 _token: _token
             },
             success:function(response){
-                console.log(response);
+                //console.log(response);
                 if(response) {
-                    //alert(response.success);
+                    //alert(response.email);
                 }
             },
             error: function(error) {
                 console.log(error);
             }
         });
-        setBox(177,313, 170,30, 'BoxTop');
-        setBox(209,325, 170,48, 'BoxMiddle');
-        setBox(319,484, 28,68, 'BoxDown');
-        setBox(329,469, 14,54, 'BottleMaker');
-        setBox(312,392, 38,54, 'BoxShelves');
-        setBox(303,364, 28,54, 'BoxDishwasher');
-        setBox(293,313, 52,54, 'BoxWashing');
-        setBox(150,274, 39,187, 'PenalFridge');
-        setBox(140,232, 42,187, 'PenalMicrowave');
-        setBox(124,154, 79,187, 'PenalShelves');
+        $(window).on('load', function(){
+            if($('.calc_body').width() < 700)
+            {
+                var y = 0;
+                var x = 0;
+                var h = 0;
+                var w = 0;
+                x= 700 - parseInt($('.calc_body').width());
+                setCoord(x);
+            }else{
+                setDefCoord();
+            }
+        })
+
+        $(window).on('resize',function (){
+            if($('.calc_body').width() < 700){
+                console.log($('.calc_body').width());
+                var y = 0;
+                var x = 0;
+                var h = 0;
+                var w = 0;
+                x= 700 - parseInt($('.calc_body').width());
+
+                console.log(parseInt($('.BoxTop').css('left')));
+                console.log(700 - parseInt($('.calc_body').width()));
+                setCoord(x);
+            }
+
+        })
+
+        function setCoord(x){
+            setBox(177 + (x/10),313 - (x/2.2), 170 - (x/4.2),30 - (x/24), 'BoxTop');
+            setBox(209 + (x/15),325 - (x/2.12), 170 - (x/4.2),48 - (x/14), 'BoxMiddle');
+            setBox(319 + (x/10),484 - (x/2.2), 28 - (x/4.2),68 - (x/24), 'BoxDown');
+            setBox(329 - (x/9),469 - (x/1.5), 14 - (x/35),54 - (x/15), 'BottleMaker');
+            setBox(312 - (x/10),392 - (x/1.8), 38 - (x/19),54 - (x/15), 'BoxShelves');
+            setBox(303 - (x/12),364 - (x/1.92), 28 - (x/24),54 - (x/15), 'BoxDishwasher');
+            setBox(293 - (x/14),313 - (x/2.2), 52 - (x/15),54 - (x/15), 'BoxWashing');
+            setBox(150 + (x/7),274 - (x/2.6), 39 - (x/15),187 - (x/3.8), 'PenalFridge');
+            setBox(140 + (x/6.7),232 - (x/3.0), 42 - (x/15),187 - (x/3.8), 'PenalMicrowave');
+            setBox(124 + (x/5.5),154 - (x/4.2), 79 - (x/11),187 - (x/3.8), 'PenalShelves');
+        }
+        function setDefCoord()
+        {
+            setBox(177,313, 170,30, 'BoxTop');
+            setBox(209,325, 170,48, 'BoxMiddle');
+            setBox(319,484, 28,68, 'BoxDown');
+            setBox(329,469, 14,54, 'BottleMaker');
+            setBox(312,392, 38,54, 'BoxShelves');
+            setBox(303,364, 28,54, 'BoxDishwasher');
+            setBox(293,313, 52,54, 'BoxWashing');
+            setBox(150,274, 39,187, 'PenalFridge');
+            setBox(140,232, 42,187, 'PenalMicrowave');
+            setBox(124,154, 79,187, 'PenalShelves');
+        }
     }
 
     initialize();
